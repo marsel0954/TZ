10af01c254cc3e8ccb7d565d7b351cb1043100f3
Марсель <marsel0954u@gmail.com>
2023-07-05 01:10:38 +0300

    добавление assertj для софт ассертов + классы для работы с базами данных

---
+++ src/test/java/db/DataBaseMain.java
@@ -0,0 +1,53 @@
+package db;
+
+import dataBase.Connection;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseMain {
+    protected final String query = "SELECT*FROM mydbautotest.datatest";
+    protected java.sql.Connection connection;
+    @BeforeTest
+    public void setUp() throws SQLException {
+        connection = DriverManager.getConnection(Connection.URL, Connection.USERNAME, Connection.PASSWORD);
+    }
+    @AfterTest
+    public void tearDown() throws SQLException {
+        connection.close();
+    }
+
+    protected List<String> showDataBase() throws SQLException {
+        ResultSet resultSet = Connection.statement.executeQuery(query);
+        List<String> results = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String resultUserName = resultSet.getString("userName");
+            String resultIduser = resultSet.getString("iduser");
+            String resultSurName = resultSet.getString("userSurname");
+            String resultAge = resultSet.getString("age");
+            results.add(resultIduser);
+            results.add(resultUserName);
+            results.add(resultSurName);
+            results.add(resultAge);
+        }
+        System.out.println("Записи в бд " + results);
+
+        return results;
+    }
+    protected void addNewUser(String iduserValue, String usernameValue, String userSurnameValue, String ageValue) throws SQLException {
+        String addUserSQL = "INSERT INTO mydbautotest.datatest (iduser, userName, userSurname, age) " +
+                "VALUES " + "('" + iduserValue + "'," + "'" + usernameValue + "'," + "'" + userSurnameValue + "',"
+                + "'" + ageValue + "');";
+
+        System.out.println(addUserSQL);
+
+        connection.createStatement().execute(addUserSQL);
+    }
+
+}
--- src/test/java/db/DatabaseTest.java
+++ src/test/java/db/DatabaseTest.java
@@ -1,46 +1,50 @@
 package db;
 
 import dataBase.Connection;
+import org.assertj.core.api.SoftAssertions;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 
-public class DatabaseTest {
-    private java.sql.Connection connection;
-    @BeforeTest
-    public void setUp() throws SQLException {
-        connection = DriverManager.getConnection(Connection.URL, Connection.USERNAME, Connection.PASSWORD);
-    }
-    @AfterTest
-    public void tearDown() throws SQLException {
-        connection.close();
-    }
-
+public class DatabaseTest extends DataBaseMain{
     @Test
     public void testFindValueInDatabase() throws SQLException {
-        String query = "SELECT*FROM mydbautotest.datatest";
-        ResultSet resultSet = Connection.statement.executeQuery(query);
-
-        List<String> results = new ArrayList<>();
-
-        while (resultSet.next()) {
-            String resultUserName = resultSet.getString("userName");
-            String resultIduser = resultSet.getString("iduser");
-            String resultSurName = resultSet.getString("userSurname");
-            results.add(resultIduser);
-            results.add(resultUserName);
-            results.add(resultSurName);
-        }
-        System.out.println("Записи в бд " + results);
-
+        List<String> results = showDataBase();
         Assert.assertTrue(results.contains("Антонов"),"Такого значения нет");
     }
+    @Test
+    public void testAddUser() throws SQLException {
+        //Проверка добавление нового сотрудника через метод в DataBaseMain
+        addNewUser("7","Артур","Пирожков", "55");
+        List<String> results = showDataBase();
+
+        SoftAssertions soft = new SoftAssertions();
+        //Проверка на содержание 'Артур' необходимо улучшить проверку
+        soft.assertThat(results).contains("Артур");
+
+        //Удаление созданного клиента
+        connection.createStatement().execute("DELETE FROM mydbautotest.datatest WHERE (iduser = 7);");
+        List<String> resultsAfterDelete = showDataBase();
+
+        //Проверка на отсутсвие 'Артур' необходимо улучшить проверку
+        soft.assertThat(resultsAfterDelete).doesNotContain("Артур");
+        soft.assertAll();
+    }
+    @Test
+    public void changeUserAttribute() throws SQLException {
+        //Изменение атрибутов пользователя
+        showDataBase();
+        connection.createStatement().execute("UPDATE mydbautotest.datatest SET age = '17' WHERE (iduser = 3)");
+        showDataBase();
+    }
+    @Test
+    public void getSomeQuery() throws SQLException {
+        //Добавление нового пользователя
+        Connection.statement.executeQuery("INSERT INTO mydbautotest.datatest (iduser, userName, userSurname, age) " +
+                "VALUES ('5', 'Александр', 'Попов', '27');");
+    }
+
 }
